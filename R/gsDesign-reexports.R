037382a6783fff9781b85759fa18a347cb4ec5d1
Keaven <temzq@yahoo.com>
2025-12-06 11:12:59 -0500

    Export sf functions, update vignette title, bump version to 0.2.0

---
+++ R/gsDesign-reexports.R
@@ -0,0 +1,66 @@
+#' @importFrom gsDesign sfBetaDist sfCauchy sfExponential sfExtremeValue
+#' @importFrom gsDesign sfExtremeValue2 sfGapped sfHSD sfLDOF sfLDPocock
+#' @importFrom gsDesign sfLinear sfLogistic sfNormal sfPoints sfPower
+#' @importFrom gsDesign sfStep sfTDist sfTrimmed sfTruncated sfXG1 sfXG2 sfXG3
+#' @export
+sfBetaDist <- gsDesign::sfBetaDist
+
+#' @export
+sfCauchy <- gsDesign::sfCauchy
+
+#' @export
+sfExponential <- gsDesign::sfExponential
+
+#' @export
+sfExtremeValue <- gsDesign::sfExtremeValue
+
+#' @export
+sfExtremeValue2 <- gsDesign::sfExtremeValue2
+
+#' @export
+sfGapped <- gsDesign::sfGapped
+
+#' @export
+sfHSD <- gsDesign::sfHSD
+
+#' @export
+sfLDOF <- gsDesign::sfLDOF
+
+#' @export
+sfLDPocock <- gsDesign::sfLDPocock
+
+#' @export
+sfLinear <- gsDesign::sfLinear
+
+#' @export
+sfLogistic <- gsDesign::sfLogistic
+
+#' @export
+sfNormal <- gsDesign::sfNormal
+
+#' @export
+sfPoints <- gsDesign::sfPoints
+
+#' @export
+sfPower <- gsDesign::sfPower
+
+#' @export
+sfStep <- gsDesign::sfStep
+
+#' @export
+sfTDist <- gsDesign::sfTDist
+
+#' @export
+sfTrimmed <- gsDesign::sfTrimmed
+
+#' @export
+sfTruncated <- gsDesign::sfTruncated
+
+#' @export
+sfXG1 <- gsDesign::sfXG1
+
+#' @export
+sfXG2 <- gsDesign::sfXG2
+
+#' @export
+sfXG3 <- gsDesign::sfXG3
