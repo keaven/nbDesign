11100ffd7b3174d8ead0df3c4c359c50efee7cca
Nan Xiao <me@nanx.me>
2025-12-14 23:15:04 -0500

    Add `@examples` and `@return` for reexports and put on one page

--- R/reexport-gsDesign.R
+++ R/reexport-gsDesign.R
@@ -1,91 +1,130 @@
+#' Objects exported from other packages
+#'
+#' These objects are imported from other packages.
+#' Follow the links below to see their documentation.
+#'
+#' @return See the original documentation in the gsDesign package for details.
+#'
+#' @examples
+#' x <- gsDesign(k = 2, test.type = 2)
+#' gsBoundSummary(x)
+#'
+#' @name reexports
+#' @docType import
+#' @keywords internal
+NULL
+
 #' @importFrom gsDesign gsDesign
+#' @rdname reexports
 #' @export
 gsDesign::gsDesign
 
 #' @importFrom gsDesign sfBetaDist
+#' @rdname reexports
 #' @export
 gsDesign::sfBetaDist
 
 #' @importFrom gsDesign sfCauchy
+#' @rdname reexports
 #' @export
 gsDesign::sfCauchy
 
 #' @importFrom gsDesign sfExponential
+#' @rdname reexports
 #' @export
 gsDesign::sfExponential
 
 #' @importFrom gsDesign sfExtremeValue
+#' @rdname reexports
 #' @export
 gsDesign::sfExtremeValue
 
 #' @importFrom gsDesign sfExtremeValue2
+#' @rdname reexports
 #' @export
 gsDesign::sfExtremeValue2
 
 #' @importFrom gsDesign sfGapped
+#' @rdname reexports
 #' @export
 gsDesign::sfGapped
 
 #' @importFrom gsDesign sfHSD
+#' @rdname reexports
 #' @export
 gsDesign::sfHSD
 
 #' @importFrom gsDesign sfLDOF
+#' @rdname reexports
 #' @export
 gsDesign::sfLDOF
 
 #' @importFrom gsDesign sfLDPocock
+#' @rdname reexports
 #' @export
 gsDesign::sfLDPocock
 
 #' @importFrom gsDesign sfLinear
+#' @rdname reexports
 #' @export
 gsDesign::sfLinear
 
 #' @importFrom gsDesign sfLogistic
+#' @rdname reexports
 #' @export
 gsDesign::sfLogistic
 
 #' @importFrom gsDesign sfNormal
+#' @rdname reexports
 #' @export
 gsDesign::sfNormal
 
 #' @importFrom gsDesign sfPoints
+#' @rdname reexports
 #' @export
 gsDesign::sfPoints
 
 #' @importFrom gsDesign sfPower
+#' @rdname reexports
 #' @export
 gsDesign::sfPower
 
 #' @importFrom gsDesign sfStep
+#' @rdname reexports
 #' @export
 gsDesign::sfStep
 
 #' @importFrom gsDesign sfTDist
+#' @rdname reexports
 #' @export
 gsDesign::sfTDist
 
 #' @importFrom gsDesign sfTrimmed
+#' @rdname reexports
 #' @export
 gsDesign::sfTrimmed
 
 #' @importFrom gsDesign sfTruncated
+#' @rdname reexports
 #' @export
 gsDesign::sfTruncated
 
 #' @importFrom gsDesign sfXG1
+#' @rdname reexports
 #' @export
 gsDesign::sfXG1
 
 #' @importFrom gsDesign sfXG2
+#' @rdname reexports
 #' @export
 gsDesign::sfXG2
 
 #' @importFrom gsDesign sfXG3
+#' @rdname reexports
 #' @export
 gsDesign::sfXG3
 
 #' @importFrom gsDesign gsBoundSummary
+#' @rdname reexports
 #' @export
 gsDesign::gsBoundSummary
